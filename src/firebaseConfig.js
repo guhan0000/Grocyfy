39913e1afe864fd01908999eb618048ef4b313cd
Guhan M <116095658+guhan0000@users.noreply.github.com>
2025-11-30 10:02:18 +0530

    Comment out firebase configuration details
    Comment out existing firebase configuration for security.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -12,15 +12,15 @@ import { getFirestore } from "firebase/firestore";
 //     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
 //     appId: "YOUR_APP_ID"
 // };
-const firebaseConfig = {
-    apiKey: "AIzaSyBr9eNgFupEHYFRdZz6Fl3YHRrxoqR9O7Q",
-    authDomain: "grocery-billing-f3b4c.firebaseapp.com",
-    projectId: "grocery-billing-f3b4c",
-    storageBucket: "grocery-billing-f3b4c.firebasestorage.app",
-    messagingSenderId: "1070977931930",
-    appId: "1:1070977931930:web:dbaf526b7d78e000155d23",
-    measurementId: "G-TS3QERWDJ2"
-};
+// const firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "grocery-billing-f3b4c.firebaseapp.com",
+//     projectId: "grocery-billing-f3b4c",
+//     storageBucket: "grocery-billing-f3b4c.firebasestorage.app",
+//     messagingSenderId: "1070977931930",
+//     appId: "1:1070977931930:web:dbaf526b7d78e000155d23",
+//     measurementId: "G-TS3QERWDJ2"
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
